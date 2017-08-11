9a4de214bacd638b992ea937bf6781f82c9ed6ee
kaalm1 <kaalm1@gmail.com>
2017-08-11 11:39:23 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,30 @@
 // Code MovieReviews Here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const Review = ({ title, img_url }) => {
+  return (
+    <div className="review">
+      <img src={img_url} />
+      <h3>{title}</h3>
+    </div>
+  )
+}
+
+const MovieReviews = ({ reviews }) => (
+  <div className="review-list">
+    {reviews.map((review) => <Review title={review.display_title} img_url={review.multimedia.src} />)}
+  </div>
+);
+
+MovieReviews.defaultProps = {
+  reviews: PropTypes.shape({
+    img_url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
+  }),
+}
+
+
+
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,52 @@
 // Code SearchableMovieReviewsContainer Here
+import React, { Component } from 'react';
+import MovieReviews from './MovieReviews'
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+            + `api-key=${NYT_API_KEY}`;
+
+
+
+class SearchableMovieReviewsContainer extends Component{
+  constructor() {
+    super();
+
+    this.state = {
+      reviews: [],
+      searchTerm: ""
+    };
+  }
+
+  componentWillMount() {
+    fetch(URL)
+      .then(response => response.json())
+      .then((reviews) => {
+          reviews = reviews.results
+          this.setState({ reviews })
+      });
+  }
+
+  updateSearch = (event) =>{
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
+  reviewsFitCritiera = (reviews, criteria)=>{
+    return reviews.filter((review)=>{
+      return review.display_title.includes(criteria)
+    })
+  }
+
+  render(){
+    return(
+      <div className="searchable-movie-reviews">
+        <input type="text" onChange={this.updateSearch}/>
+        <MovieReviews reviews={this.reviewsFitCritiera(this.state.reviews,this.state.searchTerm)} />
+      </div>
+    )
+  }
+}
+
+export default SearchableMovieReviewsContainer
